788540b64273012351c1b348fe35c55847825d8b
Tomi Vasilko <102300613+tvslk@users.noreply.github.com>
2025-02-12 11:51:30 +0100

    Add IDs to sign-in, register, and logout buttons for improved accessibility

--- parking-system/src/app/components/MainPageButtons/MainPageButtons.tsx
+++ parking-system/src/app/components/MainPageButtons/MainPageButtons.tsx
@@ -7,12 +7,14 @@ export default function MainPageButtons() {
     return (
       <div className="flex gap-4">
         <Link
+          id="signin"
           className="bg-gray-300 text-black px-6 py-2 rounded-md hover:bg-gray-400"
           href="/signin"
         >
           Sign in
         </Link>
         <Link
+          id="register"
           className="bg-gray-500 text-black px-6 py-2 rounded-md hover:bg-gray-600"
           href="/register"
         >
--- parking-system/src/app/register/page.tsx
+++ parking-system/src/app/register/page.tsx
@@ -100,6 +100,7 @@ export default function Register() {
 
             <div>
               <button
+                id='register'
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-gray-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
               >
