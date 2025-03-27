b922511d953dfa8c72e5c34d845a49db777d26f0
Tomáš Vasiľko <tomas.vasilko@kosickaakademia.sk>
2025-03-27 17:51:23 +0100

    Refactor Sidebar component to simplify authentication logic by removing unused isLoading and adminChecked states

--- parking-system/src/app/components/Sidebar/Sidebar.tsx
+++ parking-system/src/app/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,12 @@
 "use client";
-
-import { useEffect, useState } from "react";
 import { useAuthStatus } from "@/app/hooks/useAuthStatus";
 import AdminSidebar from "./AdminSidebar";
 import BaseSidebar from "./BaseSidebar";
 
-
 const Sidebar = () => {
-  const { user, isLoading, isAdmin, adminChecked } = useAuthStatus();
-  
+  const { isAdmin, user } = useAuthStatus();
   const fullName = user?.name || "User";
-  
+
   if (isAdmin) {
     return <AdminSidebar fullName={fullName} />;
   } else {
